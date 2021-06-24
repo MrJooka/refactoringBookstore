62282e8a95792bde330d16797e02590910c4dfde
MrJooka <mr.jooka@gmail.com>
2021-06-24 17:22:42 +0900

    Search Bar

---
+++ index.js
@@ -0,0 +1,11 @@
+import SearchBar from "../../components/bookstore/header/searchBar";
+
+const Bookstore = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
+
+export default Bookstore;
